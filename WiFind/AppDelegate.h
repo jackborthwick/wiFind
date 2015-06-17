06652e8d1f49efc4b573fcdd6e28262b4340812b
Jack Borthwick <jackmckeanborthwick@gmail.com>
2015-06-17 13:38:54 -0400

    Initial Commit

---
+++ WiFind/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  WiFind
+//
+//  Created by Jack Borthwick on 6/17/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ WiFind/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WiFind
+//
+//  Created by Jack Borthwick on 6/17/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
